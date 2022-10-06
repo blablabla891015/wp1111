5f5710d296146dbc8d08ffcb5475b43f85af7d89
blablabla891015 <83815454+blablabla891015@users.noreply.github.com>
2022-10-06 15:28:23 +0800

    hw3

--- hw3/src/App.js
+++ hw3/src/App.js
@@ -1,32 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import ReactDOM from 'react-dom/client';
-class abc extends React.Component{
-  constructor(){
-    this.node =document.createElement("div")
+import ReactDOM from 'react-dom';
+class Todo_items extends React.Component{
+  constructor(props){
+    super(props)
   }
   render(){
-    return <h1>hello</h1>
+    return <li className='todo-app__item'>
+      <div className='todo-app__checkbox'>
+        <input type={'checkbox'} itemID="2"></input>
+
+        <label htmlFor='2'></label>
+      </div>
+      {this.props.thing}
+      </li>
   }
+}
+class Todo_app_main extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+    lis:props.lis,
+    count_child:props.count_child,
+    keypress:props.keypress
+
+  }
+  }
+     
+  render(){
+    return <section className='todo-app__main'>
+      <input className='todo-app__input' onKeyPress={this.state.keypress} ></input>
+      <ul className='todo-app__list'>{this.state.lis}</ul>
+    </section>
+  }
+  
+}
+class Todo_app_footer extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      lis:props.lis
+    }
+  }
+  render(){
+    return <footer className='todo-app__footer'>
+    <Todo_app_total lis={this.state.lis}></Todo_app_total>
+    <ul className='todo-app__view-buttons'></ul>
+    <div className='todo-app__clean'></div>
+  </footer>
+  }
+}
+class Todo_app_total extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      lis:props.lis
+    }
+  }
+  render(){
+    return <div className='todo-app__total'>{this.state.lis.length}</div>
+  }
+}
+class Todo_app_root extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      lis:[],
+      count_child:0
+    }
+  }
+  keypress=(e)=>{
+    if(e.key==='Enter'){
+      this.state.lis.push(<Todo_items key={this.state.count_child+1} thing={e.target.value} ></Todo_items>)
+      this.setState(s=>({lis:s.lis}))
+      this.setState(s=>({count_child: s.count_child+1}))
+    }
+  }
+  render(){
+    return <div className="todo-app__root">
+    <header className="todo-app__header"></header>
+    <Todo_app_main lis={this.state.lis} count_child={this.state.count_child} keypress={this.keypress}></Todo_app_main>
+    <Todo_app_footer lis={this.state.lis}></Todo_app_footer>
+  </div>
+  }
+
 }
 function App() {
   return (
-    <div className="todo-app__root">
-      <header className="todo-app__header">
-
-      </header>
-      <section className='todo-app__main'>
-        <input className='todo-app__input'></input>
-        <ul className='todo-app__list' itemID='todo_list'></ul>
-      </section>
-      <footer className='todo-app__footer'>
-        <div className='todo-app__total'></div>
-        <ul className='todo-app__view-buttons'></ul>
-        <div className='todo-app__clean'></div>
-      </footer>
-    </div>
+    <Todo_app_root></Todo_app_root>
   );
 }
-
 export default App;
--- hw3/src/index.js
+++ hw3/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// ReactDOM.render(<Todo_app_list></Todo_app_list>,document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
