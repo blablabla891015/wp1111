ea14cbb89b9d368b701959c42d5cc91352ee04ef
blablabla891015 <83815454+blablabla891015@users.noreply.github.com>
2022-11-29 09:04:42 +0800

    hack2

--- hack2/frontend/src/App.js
+++ hack2/frontend/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
-function App() {
+// Params are placeholders in the URL that begin
+// with a colon, like the `:id` param defined in
+// the route in this example. A similar convention
+// is used for matching dynamic segments in other
+// popular web frameworks like Rails and Express.
+
+export default function App(props) {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h2>Accounts</h2>
+
+        <ul>
+          <li>
+            <Link to="/netflix">Netflix</Link>
+          </li>
+          <li>
+            <Link to="/zillow-group">Zillow Group</Link>
+          </li>
+          <li>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/modus-create">Modus Create</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/:id" element={<Child />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const Child=()=> {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams();
+  return (
+    <div>
+      <h3>ID: {id}</h3>
+    </div>
+  );
+}
--- hw6/backend/src/server.js
+++ hw6/backend/src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors'
 // import mongoose from 'mongoose';
 import db from './routes/db'
-import routes from './routes'; 
+import router from './routes/index'
 db.connect()
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
+app.use('/', router);
 
 app.listen(port, () =>
  console.log(`Example app listening on port ${port}!`),
