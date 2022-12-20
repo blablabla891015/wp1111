453adf6a92b212fbefeafa63bee7f296297e04e7
blablabla891015 <83815454+blablabla891015@users.noreply.github.com>
2022-12-20 09:38:15 +0800

    hack3

--- hack3/backend/src/mongo.js
+++ hack3/backend/src/mongo.js
@@ -2,12 +2,25 @@ import mongoose from "mongoose";
 import { dataInit } from "./upload.js";
 
 import "dotenv-defaults/config.js";
-
+import dotenv from 'dotenv-defaults'
 mongoose.set("strictQuery", true);
 
 async function connect() {
   // TODO 1 Connect to your MongoDB and call dataInit()
-
+  dotenv.config()
+  mongoose.connect(
+    process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        }
+    
+    // TODO Part I-3: connect the backend to mongoDB
+).then(async res => {
+    if (process.env.MODE === 'Reset') {
+        console.log('Reset Mode: reset the data')
+        dataInit()
+    }
+})
   // TODO 1 End
 }
 
